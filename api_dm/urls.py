fb39dd9821585f968b042c67a6b2bab00cb228d6
Tutu12344 <padde.cross@gmail.com>
2020-09-27 22:51:46 +0900

    urls create

--- api_dm/urls.py
+++ api_dm/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from api_dm import views
 app_name = "dm"
 
 router = DefaultRouter()
+# //basenameを指定しないとserializerが同じなので識別してくれない
+router.register("message",views.MessageViewSet,basename="message")
+router.register("inbox",views.InboxListView,basename="inbox")
 
 urlpatterns = [
     path('', include(router.urls))

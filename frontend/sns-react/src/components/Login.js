a97fd4d254d89f6359470e74ebcf806b91de645c
Tutu12344 <padde.cross@gmail.com>
2020-09-28 19:23:07 +0900

    Login View Create

--- frontend/sns-react/src/components/Login.js
+++ frontend/sns-react/src/components/Login.js
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { withCookies } from "react-cookie";
+import axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  span: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    color: "teal",
+  },
+  spanError: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    color: "fuchsia",
+    marginTop: 10,
+  },
+}));
 
 const Login = () => {
+  const classes = useStyles();
+
   return <div></div>;
 };
 

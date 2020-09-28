b0a33f7bae59cc33c9d01354518851410d0878cc
Tutu12344 <padde.cross@gmail.com>
2020-09-28 19:14:51 +0900

    router setting

--- frontend/sns-react/src/App.js
+++ frontend/sns-react/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import { createMuiTheme } from "@material-ui/core/styles/createMuiTheme";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import indigo from "@material-ui/core/colors/indigo";
-
+import Navbar from "./components/Navbar";
 const theme = createMuiTheme({
   palette: {
     primary: indigo,
@@ -17,7 +17,11 @@ const theme = createMuiTheme({
 });
 
 function App() {
-  return <MuiThemeProvider theme={theme}></MuiThemeProvider>;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Navbar />
+    </MuiThemeProvider>
+  );
 }
 
 export default App;

55a3f66c5e9860058679bf23aa14721f0093ed53
Tutu12344 <padde.cross@gmail.com>
2020-09-29 22:29:21 +0900

    complete

--- frontend/sns-react/src/components/main.js
+++ frontend/sns-react/src/components/main.js
@@ -5,8 +5,11 @@ import { GoMail } from "react-icons/go";
 import { BsFillPeopleFill } from "react-icons/bs";
 import Profile from "./Profile";
 import ProfileManager from "./ProfileManager";
+import Ask from "./Ask";
+import InboxDM from "./InboxDM";
+
 const Main = () => {
-  const { profiles, profile, asKList, asKListFull, inbox } = useContext(
+  const { profiles, profile, askList, askListFull, inbox } = useContext(
     ApiContext
   );
   const filterProfiles = profiles.filter((prof) => {
@@ -18,38 +21,71 @@ const Main = () => {
       <Profile
         key={filprof.id}
         profileData={filprof}
-        askData={asKListFull.filter((ask) => {
+        askData={askListFull.filter((ask) => {
           return (
-            filprof.userPro === ask.askFrom || filprof.usePro === ask.askTo
+            (filprof.userPro === ask.askFrom) | (filprof.userPro === ask.askTo)
           );
         })}
       />
     ));
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={4}>
-          <div className="app-profiles">{listProfiles}</div>
-        </Grid>
-        <Grid item xs={4}>
-          <div className="app-details">
-            <ProfileManager />
-          </div>
-          <h3 className="title-ask">
-            <BsFillPeopleFill className="badge" />
-            Approval request list
-          </h3>
-          <div className="app-details"></div>
-        </Grid>
-        <Grid item xs={4}>
-          <h3>
-            <GoMail className="badge" />
-            DM Inbox
-          </h3>
-          <div className="app-dms"></div>
-        </Grid>
+    <Grid container>
+      <Grid item xs={4}>
+        <div className="app-profiles">
+          <div className="task-list">{listProfiles}</div>
+        </div>
       </Grid>
-    </div>
+
+      <Grid item xs={4}>
+        <div className="app-details">
+          <ProfileManager />
+        </div>
+        <h3 className="title-ask">
+          {" "}
+          <BsFillPeopleFill className="badge" />
+          Approval request list
+        </h3>
+        <div className="app-details">
+          <div className="task-list">
+            <ul>
+              {profile.id &&
+                askList.map((ask) => (
+                  <Ask
+                    key={ask.id}
+                    ask={ask}
+                    prof={profiles.filter((item) => {
+                      return item.userPro === ask.askFrom;
+                    })}
+                  />
+                ))}
+            </ul>
+          </div>
+        </div>
+      </Grid>
+
+      <Grid item xs={4}>
+        <h3>
+          <GoMail className="badge" />
+          DM Inbox
+        </h3>
+        <div className="app-dms">
+          <div className="task-list">
+            <ul>
+              {profile.id &&
+                inbox.map((dm) => (
+                  <InboxDM
+                    key={dm.id}
+                    dm={dm}
+                    prof={profiles.filter((item) => {
+                      return item.userPro === dm.sender;
+                    })}
+                  />
+                ))}
+            </ul>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
